df94606505a6ff07afee05bede893a856524c5d9
Thierry <titimoby@gmail.com>
2019-05-18 18:30:42 +0200

    add fritzing and image schematics

---
+++ potentiometer/read-value.py
@@ -0,0 +1,19 @@
+import gamebuino_meta as gb
+import time
+import board
+from analogio import AnalogIn
+ 
+analog_in = AnalogIn(board.A1) 
+ 
+def get_voltage(pin):
+    # Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 3.3V)
+    # max voltage mesured 3.8V fully charged but I added a protection resistor so, it's an almost
+    return (pin.value * 3.3) / 1023.0
+ 
+while True:
+    gb.waitForUpdate()
+    gb.display.clear()
+    print(print((analog_in.value,)))
+    gb.display.setColor(gb.color.WHITE)
+    gb.display.setFontSize(3)
+    gb.display.print(8, 8, analog_in.value)
